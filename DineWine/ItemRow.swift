46895385a0be8e7bfec40593ac211c572ab992fc
srivalli93 <srivalli.kanchibotla@okstate.edu>
2025-01-16 19:56:54 -0600

    ClipShape for thumbnail

--- DineWine/ItemRow.swift
+++ DineWine/ItemRow.swift
@@ -13,8 +13,11 @@ struct ItemRow: View {
     var body: some View {
         HStack{
             Image(item.thumbnailImage)
+                .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                .overlay(Circle().stroke(Color.black, lineWidth: 2))
             VStack(alignment: .leading) {
                 Text(item.name)
+                    .font(.headline)
                 Text("$\(item.price)")
             }
         }
